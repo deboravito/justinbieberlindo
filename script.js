480a52481a0189ba314b624a4b88aa347acd8b63
deboravito <vitoria.santos.debora@escola.pr.gov.br>
2025-09-04 09:05:14 -0300

    ok

---
+++ script.js
@@ -0,0 +1,26 @@
+const avanca = document,querySelectoAll('.btn-proximo');
+const reiniciarBtn = document.getElementById9('.btn-reiniciar');
+
+avanca.onbeforematch(button =>{
+button.addEentlistener('click',function(){
+    const atual = document.querySelector('.ativo')
+    const proximo='passo-'+this.getAttribute('data-proximo);
+}
+
+
+
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+)
